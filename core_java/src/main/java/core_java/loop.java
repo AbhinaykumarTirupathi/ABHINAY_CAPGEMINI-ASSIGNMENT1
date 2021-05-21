4f8a87ae63fa26ebfbad58361341d3be8a21d368
Abhinay <tsak.tony@gmail.com>
2021-05-21 16:55:15 +0530

    hbihb

---
+++ core_java/src/main/java/core_java/loop.java
@@ -0,0 +1,20 @@
+package core_java;
+
+public class loop
+{
+	public static void main(String[] args) 
+	{
+	  for(int m=1;m<=10;m++)
+	  {
+		System.out.println("Tony");
+	  }
+	
+	//how to use while loop
+	int m=1;
+	while(m<=10)
+	{
+		System.out.println("Abhinay");
+		m++;
+	}
+	}
+}
---
+++ core_java/src/main/java/core_java/sumoftwo.java
@@ -0,0 +1,15 @@
+package core_java;
+
+public class sumoftwo {
+
+	public static void main(String[] args)
+	{
+						
+						int i=5;
+						int j=6;
+						int k=i+j;
+						
+						System.out.println("Result is "+k);
+
+}
+}
