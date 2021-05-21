6b9dc6906dee76afbe2ffea14fd096dd6092fa04
Abhinay <tsak.tony@gmail.com>
2021-05-21 16:31:55 +0530

    xdfbsb

---
+++ core_java/src/main/java/core_java/fsknf.java
@@ -0,0 +1,17 @@
+package core_java;
+
+public class Fireloop  {
+
+	public static void main(String[] args) {
+		for(int m=1;m<=10;m++) {
+			System.out.println("Abhinay Kumar Tirupathi");
+		}
+		
+		int m=1;
+		while(m<=10) {
+			System.out.println("Abhinay Kumar Tirupathi");
+			m++;
+		}
+	}
+		
+		
