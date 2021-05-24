9d5370887ce80dc7bf1bda42b9809c015fc0ed9e
Abhinay <tsak.tony@gmail.com>
2021-05-24 10:23:44 +0530

    xfcb

---
+++ core_java/src/main/java/core_java/primdat.java
@@ -0,0 +1,36 @@
+package core_java;
+
+public class primdat
+{
+
+
+		    //variable inside class is known as global variable
+		    int i;   //instance variable
+		    static int j; //class variable
+		    
+		    //notes : by default system will give default value to global variable not to local variable 
+
+		 
+
+		    public static void main(String[] args)
+		    {
+		        //variable inside a method/function is known as local variable
+		        int k=8;
+		       ;
+		        System.out.println(k);
+		        
+		        //since i want to access global variable i which is non static inside static void main 
+		        //so i have to create object of Variable class
+		        primdat v=new primdat(); //create object of class Variable
+		        System.out.println(v.i);
+		        
+		        System.out.println(j);
+
+		 
+
+		    }
+
+		 
+
+		}
+		
